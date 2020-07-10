0862f27a0c62826fc8bd0be2f18feb02269773a0
Zachary A Kniffen <zacharyakniffen@gmail.com>
2020-07-09 20:31:26 -0500

    Finished Studio.  It seems pretty barebones for the application, but I guess this is all we needed.

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/data/UserRepository.java
@@ -0,0 +1,15 @@
+package org.launchcode.javawebdevtechjobsauthentication.models.data;
+
+import org.launchcode.javawebdevtechjobsauthentication.models.Job;
+import org.launchcode.javawebdevtechjobsauthentication.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+}
